9108fd2a674de7d88ed295bd4fd9d27f460b8927
eminjano <emindilbar@gmail.com>
2022-02-21 19:19:58 +0100

    user session NGRX

---
+++ libs/users/src/lib/state/users.selectors.ts
@@ -0,0 +1,18 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { USERS_FEATURE_KEY, UsersPartialState, UsersState } from './users.reducer';
+
+// Lookup the 'Users' feature state managed by NgRx
+export const getUsersState = createFeatureSelector<UsersPartialState, UsersState>(
+  USERS_FEATURE_KEY
+);
+
+export const getUser = createSelector(getUsersState, (state) => state.user);
+
+export const getUserIsAuth = createSelector(getUsersState, (state) => state.isAuthenticated);
+export function getAllUsers(state: any) {
+  throw new Error('Function not implemented.');
+}
+
+export function getSelected(state: any) {
+  throw new Error('Function not implemented.');
+}
